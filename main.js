79df7960185ea618b411feac5210933d68bb7c02
Bodhayan429 <72012606+Bodhayan429@users.noreply.github.com>
2020-11-23 12:07:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,68 +1,30 @@
-canvas= document.getElementById('myCanvas');
-ctx= canvas.getContext("2d");
-car_width=100;
-car_height=90;
-background_image="racing.jpg";
-car_image1="car1.png";
-car_image2="car2.png"
-car1_x=10;
-car1_y=10;
-car2_x=10;
-car2_y=5;
-function add() {
-    background_imgTag= new Image();
-    background_imgTag.onload= uploadBackground;
-    background_imgTag.src= background_image;
-    car1_imgTag= new Image();
-    car1_imgTag.onload= uploadCar1;
-    car1_imgTag.src= car_image1;
-    car2_imgTag= new Image();
-    car2_imgTag.onload= uploadCar2;
-    car2_imgTag.src= car_image2;
+var canvas=new fabric.canvas('myCanvas');
+player_x=10;
+player_y=10;
+block_image_width= 30;
+block_image_height= 30;
+var player_object="";
+var block_image_object="";
+function player_update() {
+    fabric.Image.fromURL("player.png",function(Img){
+        player_object=Img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top: player_y, left: player_x
+        });
+        canvas.add(block_image_object);
+    });
 }
-function uploadBackground() {
-    ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);
-}
-function uploadCar1() {
-    ctx.drawImage(car_img1Tag,car1_x,car1_y,car_width,car_height);
-}
-function uploadCar2() {
-    ctx.drawImage(car_img2Tag,car2_x,car2_y,car_width,car_height);
-}
-window.addEventListener("keydown",my_keydown);
-function my_keydown(e) {
-    keyPressed= e.keyCode;
-    console.log(keyPressed);
-    if (keyPressed == '38') {
-        car1_up();
-        console.log("up");
-    }
-    if (keyPressed == '40') {
-        car1_down();
-        console.log("down");
-    }
-    if (keyPressed == '37') {
-        car1_left();
-        console.log("left");
-    }
-    if (keyPressed == '39') {
-        car1_right();
-        console.log("right");
-    }
-    if (keyPressed == '87') {
-        car2_up();
-        console.log("up");
-    }
-    if (keyPressed == '65') {
-        car2_left();
-        console.log("left");
-    }
-    if (keyPressed == '83') {
-        car2_down();
-        console.log("down");
-    }
-    if (keyPressed == '68') {
-        car2_right();
-        console.log("right");
-    }
-}
+function new_image(get_image) {
+    fabric.Image.fromURL(get_image,function(Img){
+        block_image_object=Img;
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.scaleToHeight(block_image_height);
+        block_image_object.set({
+            top: player_y,
+             left: player_x
+        });
+        canvas.add(block_image_object);
+    })
+} 
