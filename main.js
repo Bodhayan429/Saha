1d44c14a2b05e3bbe95d9eca25939d43b4884417
Virtual Uv <72012606+Bodhayan429@users.noreply.github.com>
2021-05-05 12:51:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,18 @@
 function setup() {
-  canvas = createCanvas(300, 300);
-  canvas.center();
-  video = createCapture(VIDEO);
-  video.hide();
-  classifier = ml5.imageClassifier('MobileNet',modleLoaded);
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
-function modleLoaded() {
-  console.log('Model Loaded');
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
 }
 
-function draw() {
-  image(video, 0, 0, 300, 300);
-  classifier.classify(video, gotResult);
-}
-
-function gotResult(error, results) {
-  if (error) {
-    console.error(error);
-  }else {
-    console.log(results);
-    document.getElementById("result_object_name").innerHTML = results[0].label
-    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
-  }
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+    }
 }
