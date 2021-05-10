0d6116a677044ecc59cb753ea0d15d00d3a17ae4
Virtual Uv <72012606+Bodhayan429@users.noreply.github.com>
2021-05-10 12:55:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -14,5 +17,17 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
     }
+}
+
+function draw() {
+    background('#5285d1');
+    document.getElementById("square_side").innerHTML = "Font size of the text will be = " + difference + "px"
+    textSize(difference);
+    fill("#f09c2e");
+    text('Bodhayan', 50, 400);
 }
